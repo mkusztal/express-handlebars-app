21e1de89bede5ba45d7e43d6b30adb9b3e59804f
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-09 16:19:49 +0300

    Add files with layout

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.engine(
+  'hbs',
+  hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' })
+);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+//request of name placeholder from `hello.hbs`
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
